ef0aa4e88bf3ed2595aa04f54184064c712a3354
Kaeden Wieberdink <kaeden.wieberdink@gmail.com>
2023-07-02 01:46:17 -0500

    project setup

--- src/main/java/com/kaedenphoto/kaedenrayphoto/KaedenRayPhotoApplication.java
+++ src/main/java/com/kaedenphoto/kaedenrayphoto/KaedenRayPhotoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KaedenRayPhotoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KaedenRayPhotoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KaedenRayPhotoApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/kaedenphoto/kaedenrayphoto/services/Keys.java
@@ -0,0 +1,9 @@
+package com.kaedenphoto.kaedenrayphoto.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Keys {
+
+
+}
